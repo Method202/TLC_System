4f070ecea23b354803a4b242f79458683f169c7c
Tartan <tartanarmour202@gmail.com>
2025-08-22 14:49:50 +0300

    added app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('studentdash.html')  # Change to the template you want
+
+if __name__ == '__main__':
+    app.run(debug=True)
