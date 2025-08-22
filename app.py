46cd4d30e84607e3fc0eead1aa7cc5b74b051b4b
Tartan <tartanarmour202@gmail.com>
2025-08-22 15:42:48 +0300

    static folder

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('studentdash.html')  # Change to the template you want
+    return render_template('admindash.html')  # Change to the template you want
 
 if __name__ == '__main__':
     app.run(debug=True)
